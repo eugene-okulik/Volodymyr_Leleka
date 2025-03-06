c0579cec5ba260b847bd5befa9807c2746e96833
Volodymyr Leleka <volodumur.leleka@gmail.com>
2025-03-06 21:15:34 +0100

    homework_3 tasks

--- homework/volodymyr_leleka/volodymyr_leleka_homework_3/task_1.py
+++ homework/volodymyr_leleka/volodymyr_leleka_homework_3/task_1.py
@@ -1,5 +1,8 @@
 a = int(input("Enter first num: "))
 b = int(input("Enter second num: "))
 print(a + b)
-print(a / b)
+if b == 0:
+    print("Nope:) (b != 0)")
+else:
+    print(a / b)
 print(a * b)
---
+++ homework/volodymyr_leleka/volodymyr_leleka_homework_3/task_3.py
@@ -0,0 +1,7 @@
+from cmath import sqrt
+
+first_num = int(input("Enter first num: "))
+second_num = int(input("Enter second num: "))
+#geometric = sqrt(first_num*second_num)
+print("Result arifmetic:",(first_num+second_num) / 2)
+print("Result geometric:", sqrt(first_num*second_num))
