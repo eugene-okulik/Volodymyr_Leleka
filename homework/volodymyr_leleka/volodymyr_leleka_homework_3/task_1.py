5a89f5707045d5c7cd1e764e0fabb1bcd1b9166a
Volodymyr Leleka <volodumur.leleka@gmail.com>
2025-03-06 21:19:45 +0100

    homework_3 tasks

--- homework/volodymyr_leleka/volodymyr_leleka_homework_3/task_1.py
+++ homework/volodymyr_leleka/volodymyr_leleka_homework_3/task_1.py
@@ -1,8 +1,9 @@
 a = int(input("Enter first num: "))
 b = int(input("Enter second num: "))
-print(a + b)
-if b == 0:
-    print("Nope:) (b != 0)")
-else:
-    print(a / b)
-print(a * b)
+print("Sum =", a + b)
+#if b == 0:
+#    print("Nope:) (b != 0)")
+#else:
+#    print(a / b)
+print("Raz =", a-b)
+print("Proisvod =", a * b)
