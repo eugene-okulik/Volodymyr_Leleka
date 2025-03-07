9338f78cf1a461bf7ac341d0dae12e33ba30bba4
Volodymyr Leleka <volodumur.leleka@gmail.com>
2025-03-07 11:25:11 +0100

    homework_3 edit code after check

--- homework/volodymyr_leleka/volodymyr_leleka_homework_3/task_3.py
+++ homework/volodymyr_leleka/volodymyr_leleka_homework_3/task_3.py
@@ -2,5 +2,5 @@ from cmath import sqrt
 
 first_num = int(input("Enter first num: "))
 second_num = int(input("Enter second num: "))
-print("Result arifmetic:", (first_num+second_num) / 2)
-print("Result geometric:", sqrt(first_num*second_num))
+print("Result arifmetic:", (first_num + second_num) / 2)
+print("Result geometric:", sqrt(first_num * second_num))
