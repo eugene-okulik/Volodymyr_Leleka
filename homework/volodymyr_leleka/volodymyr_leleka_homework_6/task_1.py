fe8badd29bcbe8e559ddaf30b8e300a09bb660f8
Volodymyr Leleka <volodumur.leleka@gmail.com>
2025-05-26 15:23:32 +0200

    Home work 6 v1

---
+++ homework/volodymyr_leleka/volodymyr_leleka_homework_6/task_1.py
@@ -0,0 +1,13 @@
+text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at,'
+        ' dignissim vitae libero')
+words = text.split()
+for word in words:
+        if word[-1] in ',':
+                symbol = word[-1]
+                word = word[:-1]
+        elif word[-1] in '.':
+                symbol = word[-1]
+                word = word[:-1]
+        else:
+                symbol = ''
+        print(word + 'int' + symbol, end=' ')
---
+++ homework/volodymyr_leleka/volodymyr_leleka_homework_6/task_2.py
@@ -0,0 +1,11 @@
+num = 0;
+for number in range (1,101):
+    if number % 3 == 0 and number % 5 != 0:
+        print('Fuzz')
+    elif number % 5 == 0 and number % 3 != 0:
+        print('Buzz')
+    elif number % 3 == 0 and number % 5 == 0:
+        print('FuzzBuzz')
+    else:
+        print(number)
+    num = num + 1;
