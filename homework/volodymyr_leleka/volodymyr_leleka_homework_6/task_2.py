b75ea53d230f395ff75fde10e1caf3e69f780354
Volodymyr Leleka <volodumur.leleka@gmail.com>
2025-05-26 15:38:28 +0200

    Home work 6 v2

--- homework/volodymyr_leleka/volodymyr_leleka_homework_6/task_2.py
+++ homework/volodymyr_leleka/volodymyr_leleka_homework_6/task_2.py
@@ -1,5 +1,5 @@
-num = 0;
-for number in range (1,101):
+num = 0
+for number in range(1, 101):
     if number % 3 == 0 and number % 5 != 0:
         print('Fuzz')
     elif number % 5 == 0 and number % 3 != 0:
@@ -8,4 +8,4 @@ for number in range (1,101):
         print('FuzzBuzz')
     else:
         print(number)
-    num = num + 1;
+    num = num + 1
