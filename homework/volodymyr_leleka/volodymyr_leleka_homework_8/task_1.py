892d5a3c001aa03f11272fc8a3c193d1e2da9a88
VolodymyrLeleka <volodumur.leleka@gmail.com>
2026-06-16 18:43:15 +0200

    homework_8 fixed formatting

--- homework/volodymyr_leleka/volodymyr_leleka_homework_8/task_1.py
+++ homework/volodymyr_leleka/volodymyr_leleka_homework_8/task_1.py
@@ -1,15 +1,14 @@
 import random
+
 bonus =[True, False]
 bonus = random.choice(bonus)
 salary = int(input('введите зарплату: '))
 bonus_mon = random.randint(1, 99999)
 while True:
-    if bonus == True:
+    if bonus is True:
         ful_salary = salary + bonus_mon
         print(f' {salary}, {bonus},- ${ful_salary}')
         break
     else:
         print(salary, bonus)
         break
-
-
--- homework/volodymyr_leleka/volodymyr_leleka_homework_8/task_2.py
+++ homework/volodymyr_leleka/volodymyr_leleka_homework_8/task_2.py
@@ -2,13 +2,15 @@ import sys
 sys.set_int_max_str_digits(100000)
 
 index = {5, 200, 1000, 100000}
+
+
 def progression():
     first_num = 0
     second_num = 1
-
     while True:
         yield first_num
         first_num, second_num = second_num, first_num + second_num
+
 
 count = 0
 for num in progression():
