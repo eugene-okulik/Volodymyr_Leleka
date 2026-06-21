fb6555014a404c0047c3727aa67cfbab19a956f2
VolodymyrLeleka <volodumur.leleka@gmail.com>
2026-06-21 17:40:18 +0200

    homework_9 fixed formatting

--- homework/volodymyr_leleka/volodymyr_leleka_homework_9/task_2.py
+++ homework/volodymyr_leleka/volodymyr_leleka_homework_9/task_2.py
@@ -1,8 +1,10 @@
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25,
                 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
 
+
 def filter_temp(x):
-       return x>=28
+    return x >= 28
+
 
 hot_temp = list(filter(filter_temp, temperatures))
 print(hot_temp)
